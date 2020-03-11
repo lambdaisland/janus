c505ffc25d0eedcb6e351b351da11dcf4753a7e9
evinasgu <enyert.watson@gmail.com>
2020-03-11 18:06:27 -0500

    Fix for repeated elements in parser

--- src/lambdaisland/janus/handler/flexmark.clj
+++ src/lambdaisland/janus/handler/flexmark.clj
@@ -1,7 +1,8 @@
 (ns lambdaisland.janus.handler.flexmark
   (:require [lambdaisland.janus.util :as util])
   (:import [com.vladsch.flexmark.ast Heading]
-           [com.vladsch.flexmark.ast BulletList]))
+           [com.vladsch.flexmark.ast BulletList]
+           [com.vladsch.flexmark.ast BulletListItem]))
 
 (defn extract-version-data [node]
   (-> node
@@ -14,6 +15,14 @@
    (= (type node) com.vladsch.flexmark.ast.Heading)
    (= (.getLevel node) 1))) ;; In CHANGELOG domain everything with "# " or level 1 heading is version info
 
+(defn is-segment? [node]
+  (and
+   (= (type node) com.vladsch.flexmark.ast.Heading)
+   (= (.getLevel node) 2)))
+
+(defn transform-to-list [iterable-node]
+  (iterator-seq (.iterator iterable-node)))
+
 (defn object-to-text [node]
   (.toString (.getChars node)))
 
@@ -23,7 +32,8 @@
            found? false]
       (if (or (nil? aux-node) (= found? true))
         aux-node
-        (let [shared-node (when (not (nil? aux-node)) (.getNext aux-node))]
+        (let [shared-node (when (and (not (nil? aux-node)) (is-segment? (.getNext aux-node)))
+                            (.getNext aux-node))]
           (recur shared-node (if (not (nil? shared-node))
                                (= (object-to-text shared-node) tag-repr)
                                false)))))))
@@ -36,8 +46,22 @@
           (.getNext component))]
     (if (and (not (nil? bullet-list))
              (= (type bullet-list) com.vladsch.flexmark.ast.BulletList))
+      ;; (map (fn [x] (object-to-text x))
+      ;;      (iterator-seq (.iterator (.getChildren bullet-list))))
+      ;; (loop [bullet-item (.getFirstChild bullet-list)
+      ;;        result (list)]
+      ;;   (if (or (nil? bullet-item)
+      ;;           (not (= (type bullet-item) com.vladsch.flexmark.ast.BulletListItem)))
+      ;;     result
+      ;;     (recur
+      ;;      (let [next-item (.getNext bullet-item)]
+      ;;        (if (= (type next-item) com.vladsch.flexmark.ast.BulletListItem)
+      ;;          next-item
+      ;;          nil))
+      ;;      (conj result (object-to-text bullet-item)))))
       (map (fn [x] (object-to-text x))
-           (iterator-seq (.iterator (.getChildren bullet-list))))
+           (filter (fn [x] (= (type x) com.vladsch.flexmark.ast.BulletListItem))
+              (transform-to-list (.getDescendants bullet-list))))
       (list))))
 
 (defn extract-changes [node]
